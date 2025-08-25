d0b82c64af31aa0c515df2ce5905bdd3cb1719b2
Javier Garin <hlm.javiergarin@gmail.com>
2025-08-25 13:58:35 -0400

    update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,11 +15,17 @@ const Header = async () => {
 
   return (
     <Container className="max-w-5xl left-1/2 -translate-x-1/2 fixed z-10 top-4">
-      <header className="flex justify-between items-center min-h-16 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border dark:border-border rounded-xl px-4 py-3 shadow-lg">
-        <Logo />
-        <div className="flex items-center gap-x-4">
-          <ThemeSwitcher />
-          {!session ? <Link href='/login'><Button variant={'secondary'}>Login</Button></Link> : <UserMenu auth={session} />}
+      <header className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border dark:border-border rounded-xl px-4 py-3 shadow-lg">
+        <div className="flex items-center justify-between">
+          <div className="flex-1 flex justify-start">
+            <ThemeSwitcher />
+          </div>
+          <div className="flex-1 flex justify-center">
+            <Logo />
+          </div>
+          <div className="flex-1 flex justify-end">
+            {!session ? <Link href='/login'><Button variant={'secondary'}>Login</Button></Link> : <UserMenu auth={session} />}
+          </div>
         </div>
       </header>
     </Container>
